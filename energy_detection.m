a28c4bce55e9ac23159b7f71614e8e2691442f3d
tranhp98 <50114999+tranhp98@users.noreply.github.com>
2019-11-13 00:56:50 -0500

    Update energy_detection.m

--- energy_detection.m
+++ energy_detection.m
@@ -1,53 +1,59 @@
-dig_signal = 1;
+dig_signal = 0;
 Pfa = 0.3;
 N = 500;
-snr_dB = -9;
-disp(detection(dig_signal, Pfa, N, snr_dB));
+[guess, prob] = detection(dig_signal, Pfa, N);
+fprintf('Guess is %d\n', guess)
+fprintf('Level of confidence is %.2f', prob)
 
-function prob = detection(dig_signal, Pfa, N, snr_dB)
-    % generate BPSK signal
-    d= dig_signal;
-    b=2*d-1; % Convert unipolar to bipolar
-    T=1; % Bit duration
-    Eb=T/2; % This will result in unit amplitude waveforms
-    fc=3/T; % Carrier frequency
-    t=linspace(0,5,N); % discrete time sequence between 0 and 5*T (500 samples)
-    Nsb=N/length(d); % Number of samples per bit
-    dd=repmat(d',1,Nsb); % replicate each bit Nsb times
-    bb=repmat(b',1,Nsb); dw=dd'; % Transpose the rows and columns
-    dw=dw(:)'; 
-    % Convert dw to a column vector (colum by column) and convert to a row vector
-    bw=bb';
-    bw=bw(:)'; % Data sequence samples
-    w=sqrt(2*Eb/T)*cos(2*pi*fc*t); % carrier waveform
-    bpsk_w=bw.*w; % modulated waveform
-    snr = 10.^(snr_dB./10);
+function [guess,prob] = detection(dig_signal, Pfa, N)
+    fc = 1e6;
+    t=linspace(0,5,N);
+    if dig_signal == 1
+        s = (1/2)*sin(1e6*2*pi*t);
+    else
+        s = 0;
+    end
     Pd = zeros(1,N);
+    Pabs = zeros(1,N);
+    guess1 = zeros(1,N);
+    guess0 = zeros(1,N);
  for m = 1: N
-    num=0;
+    numd=0;
+    numabs = 0;
     for i = 1: N
          %-----AWGN noise with mean 0 and variance -----%
         noise = randn(1,N);
-        v = var(noise);
-        s = sqrt(snr)*bpsk_w;
         y = s + noise;
         energy = abs(y).^2;
         energy_fin =(1/N).*sum(energy);
-        threshold(m) = (qfuncinv(Pfa)./sqrt(N/2))+v;
-        if energy_fin >= threshold(m) % Check whether the received energy is greater than threshold, if so, increment Pd (Probability of detection) counter by 1
-            num = num+1;
+        threshold(m) = (qfuncinv(Pfa)./sqrt(N/2))+1;
+        if energy_fin >= threshold(m)   % Check whether the received energy is greater than threshold, if so, increment Pd (Probability of detection) counter by 1
+           guess1(1,m) = guess1(1,m)+1;
+            if dig_signal == 1 
+                numd = numd+1;
+            end
+        end
+        if energy_fin < threshold(m) 
+            guess0(1,m) = guess0(1,m)+1;
+            if dig_signal == 0
+                numabs = numabs+1;
+            end
         end
     end
-    Pd(1,m) = num/N;
+    Pd(1,m) = numd/N;
+    Pabs(1,m) = numabs/N;
  end 
     Pd = sum(Pd)/N;
-    Pabs = 1 - Pfa;
-    if dig_signal == 1
+    Pabs = sum(Pabs)/N;
+    guess1 = sum(guess1)/N;
+    guess0 = sum(guess0)/N;
+    if guess1 > guess0
+        guess = 1;
         prob = Pd;
     else
+        guess =0;
         prob = Pabs;
     end
-    
     
   
     
